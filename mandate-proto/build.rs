d6a0871f583351cfe4797315b598e5a0d14322a6
Ryder Freeman <RyderFreeman4Logos@gmail.com>
2025-12-09 22:06:37 -0800

    feat(proto): modularize mandate grpc definitions

--- mandate-proto/build.rs
+++ mandate-proto/build.rs
@@ -1,9 +1,19 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     std::env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path().unwrap());
 
+    let protos = [
+        "../protos/common.proto",
+        "../protos/event.proto",
+        "../protos/ring.proto",
+        "../protos/group.proto",
+        "../protos/member.proto",
+        "../protos/billing.proto",
+        "../protos/auth.proto",
+    ];
+
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
-        .compile_protos(&["../protos/event.proto"], &["../protos"])?;
+        .compile_protos(&protos, &["../protos"])?;
     Ok(())
 }
